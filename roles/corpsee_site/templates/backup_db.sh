d45aea01d61e7fa552d14c23968109c38010a41b
Dmitry Khomutov <poisoncorpsee@gmail.com>
2018-08-19 16:34:40 +0700

    Fixed DB backup scripts.

--- roles/corpsee_site/templates/backup_db.sh
+++ roles/corpsee_site/templates/backup_db.sh
@@ -2,5 +2,5 @@
 
 CURRENT_DATE=`date +%Y-%m-%d`
 
-COMMAND="pg_dump -O --column-inserts -F p -U {{ corpsee_site_db_user }} -d {{ corpsee_site_db_name }} -h localhost | gzip > ./backup/{{ corpsee_site_db_name }}_${CURRENT_DATE}.sql.gz"
+COMMAND="pg_dump -O --column-inserts -F p -U {{ corpsee_site_db_user }} -d {{ corpsee_site_db_name }} -h localhost | grep -v -E '^(CREATE\ EXTENSION|COMMENT\ ON)' | gzip > ./backup/{{ corpsee_site_db_name }}_${CURRENT_DATE}.sql.gz"
 eval "${COMMAND}"
--- roles/php_censor/templates/backup_db.sh
+++ roles/php_censor/templates/backup_db.sh
@@ -2,5 +2,5 @@
 
 CURRENT_DATE=`date +%Y-%m-%d`
 
-COMMAND="pg_dump -O --column-inserts -F p -U {{ php_censor_db_user }} -d {{ php_censor_db_name }} -h localhost | gzip > ./backup/{{ php_censor_db_name }}_${CURRENT_DATE}.sql.gz"
+COMMAND="pg_dump -O --column-inserts -F p -U {{ php_censor_db_user }} -d {{ php_censor_db_name }} -h localhost | grep -v -E '^(CREATE\ EXTENSION|COMMENT\ ON)' | gzip > ./backup/{{ php_censor_db_name }}_${CURRENT_DATE}.sql.gz"
 eval "${COMMAND}"
