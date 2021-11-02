ba39cf3fa129412537bb29835130708473ca1fa5
Dmitry Khomutov <poisoncorpsee@gmail.com>
2021-11-02 19:58:44 +0700

    Fixes +.

--- roles/corpsee_site/templates/backup_db_daily.sh
+++ roles/corpsee_site/templates/backup_db_daily.sh
@@ -2,7 +2,7 @@
 
 CURRENT_DATE=$(date +%Y-%m-%d)
 
-mkdir --mode='u-x,go-x-w,u=rwX,go=rX' "${HOME}/backup"
+mkdir --parents --mode='u-x,go-x-w,u=rwX,go=rX' "${HOME}/backup"
 
 COMMAND="pg_dump -O --column-inserts -F p -U {{ corpsee_site_db_user }} -d {{ corpsee_site_db_name }} -h 127.0.0.1 | grep -v -E '^(CREATE\ EXTENSION|COMMENT\ ON)' | xz > ${HOME}/backup/{{ corpsee_site_db_name }}_${CURRENT_DATE}.sql.xz"
 eval "${COMMAND}"
